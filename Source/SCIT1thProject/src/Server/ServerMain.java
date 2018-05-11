c0f7ff50b13bd909f9122323bfe7f0390ffba707
Rensia <lion7569@gmail.com>
2018-05-11 20:56:43 +0900

    parser update
    parser

--- Source/SCIT1thProject/src/Server/ServerMain.java
+++ Source/SCIT1thProject/src/Server/ServerMain.java
@@ -1,7 +1,11 @@
 package Server;
 
+import java.util.ArrayList;
+
 import Parser.EventDayParser;
 import Parser.WeatherPlanetParser;
+import VO.EventDay;
+import VO.Weather;
 
 public class ServerMain {
 
@@ -12,8 +16,18 @@ public class ServerMain {
 		
 		
 		// 파싱 테스트용
-		new EventDayParser();
-		new WeatherPlanetParser().parshing();
+		EventDayParser eventDayParser=new EventDayParser();
+		eventDayParser.parshing();
+		ArrayList<EventDay> eventList=eventDayParser.getEventList();
+		for(int i=0;i<eventList.size();i++) {
+			System.out.println(eventList.get(i));
+		}
+		WeatherPlanetParser weatherParser=new WeatherPlanetParser();
+		weatherParser.parshing();
+		ArrayList<Weather> weatherList=weatherParser.getWeatherList();
+		for(int i=0;i<weatherList.size();i++) {
+			System.out.println(weatherList.get(i));
+		}
 	}
 
 }

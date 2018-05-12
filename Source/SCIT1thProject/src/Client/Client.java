9fcd96ac515417422286cc5339c4c0cfcb31b43b
Rensia <lion7569@gmail.com>
2018-05-13 00:46:23 +0900

    서버클라1차
    ㅇ

--- Source/SCIT1thProject/src/Client/Client.java
+++ Source/SCIT1thProject/src/Client/Client.java
@@ -1,5 +1,37 @@
 package Client;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Client {
+	public static void main(String[] args) {
+		BufferedReader reader = null;
+		PrintWriter writer = null;
+		try {
+			Socket socket = new Socket("localhost", 7777);
+			reader = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
+			writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8"));
+			Scanner ki = new Scanner(System.in);
+			writer.write(ki.nextLine());
+			writer.flush();
+			System.out.println(reader.readLine());
+			socket.close();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		} finally {
+			try {
+				reader.close();
+				writer.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
 
 }
--- Source/SCIT1thProject/src/Server/ServerMain.java
+++ Source/SCIT1thProject/src/Server/ServerMain.java
@@ -1,46 +1,43 @@
 package Server;
 
 import java.io.BufferedReader;
-import java.io.File;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import Parser.EventDayParser;
-import Parser.WeatherPlanetParser;
-import VO.EventDay;
-import VO.Weather;
 
 public class ServerMain {
 
-    public static void main(String[] args) {
-        try {
-            ServerSocket serverSocket = new ServerSocket(1234);
-            while(true) {
-            Socket socket = serverSocket.accept();
-            BufferedReader si=new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
-            PrintWriter so= new PrintWriter(new OutputStreamWriter(socket.getOutputStream(),"UTF-8"));
-
-            if(si.readLine()=="A"){
-                so.println(getDirContent());
-
-            } else {
-                so.println(si.readLine());
-            }
-             so.flush(); si.close(); so.close(); socket.close();
-            }
-        } catch(Exception e) {}
-
-    }
-    private static String getDirContent() {
-        File dir=new File("C:/");
-        String files[]=dir.list();
-        List<String> list=Arrays.asList(files);
-        return list.toString();
-    }
+	public static void main(String[] args) {
+		ServerSocket serverSocket = null;
+		BufferedReader reader = null;
+		PrintWriter writer = null;
+		try {
+			while (true) {
+				serverSocket = new ServerSocket(7777);
+				Socket socket = serverSocket.accept();
+				System.out.println("connect HAM");
+				reader = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
+				writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8"));
+				System.out.println("ok");
+				writer.print(reader.readLine());
+				System.out.println();
+				writer.flush();
+				socket.close();
+			}
+		} catch (Exception e) {
+			e.getStackTrace();
+		} finally {
+			try {
+				serverSocket.close();
+				reader.close();
+				writer.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
 }

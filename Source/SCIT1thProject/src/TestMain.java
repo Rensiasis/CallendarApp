76a09c730f2630f53e840f1671c19a4afc25ca29
Rensia <lion7569@gmail.com>
2018-05-14 20:21:48 +0900

    달력 이벤트 저장추가
    ㅇㅇ

---
+++ Source/SCIT1thProject/src/TestMain.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+import Parser.EventDayParser;
+import VO.EventDay;
+
+public class TestMain {
+	public static void main(String[] args) {
+		ArrayList<EventDay> list = EventDayParser.parshing("e", "2018");
+		for (int i = 0; i < list.size(); i++) {
+			System.out.println(list.get(i));
+		}
+	}
+}

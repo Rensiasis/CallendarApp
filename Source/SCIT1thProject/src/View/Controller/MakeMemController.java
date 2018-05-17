ceb940205887bbd86379c56fd9e8d2fda9fc8677
KYUHO CHOI <chlrbghdla@naver.com>
2018-05-17 16:34:51 +0900

    우편번호도 수정
    ㅇㅇ

--- Source/SCIT1thProject/src/View/Controller/MakeMemController.java
+++ Source/SCIT1thProject/src/View/Controller/MakeMemController.java
@@ -106,7 +106,8 @@ public class MakeMemController implements Initializable {
 
 				if (Chk_true_agree.isSelected()) {
 					if (Input_ID.getText().length() == 0 || Input_Pass.getText().length() == 0
-							|| postNum.getText().length() == 0 || detailAddress.getText().length() ==0) {
+							|| postNum.getText().length() == 0 || detailAddress.getText().length() ==0
+							|| Chk_Gender.getValue() == null) {
 						Alert alert = new Alert(AlertType.ERROR);
 						alert.setTitle("회원가입 실패");
 						alert.setHeaderText("회원가입에 실패하셨습니다.");
